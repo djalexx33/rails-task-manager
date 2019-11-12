e09a0914d60e19c23043c4170847df788890055f
Alexandre BARAT <alex.barat@gmail.com>
2019-11-12 18:55:50 +0100

    Rails task manager

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2019_11_12_111157) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed", default: true
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
